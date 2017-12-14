7ec771c62d47a952ddf6d22b4cece67e175963d3
099111 <099111@NTTDATA608404.dot.state.tx.us>
2017-12-14 14:46:48 -0600

    Added rating to form and display

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 angular.module("surveyApp", []).controller("surveyCtrl", [
   "$scope",
   function($scope) {
-    $scope.lists = [{ category: "Frontend", skill: "AngularJS" }];
+    $scope.lists = [{ category: "Frontend", skill: "AngularJS", rate: "3" }];
 
     $scope.addToList = function() {
-      $scope.lists.push({ category: $scope.newCat, skill: $scope.newSkill });
+      $scope.lists.push({ category: $scope.newCat, skill: $scope.newSkill, rate: $scope.newRate });
     };
   }
 ]);
